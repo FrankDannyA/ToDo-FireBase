ebe9e2736704b1f42e546a21233197e06ae31632
Daniil <orgfrank@icloud.com>
2021-12-24 12:04:46 +0600

    Реализовал функцию авторизации

--- ToDo + FireBase/Controllers/FolderViewController.swift
+++ ToDo + FireBase/Controllers/FolderViewController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import Firebase
 
 class FolderViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
            
@@ -18,5 +22,20 @@ class FolderViewController: UIViewController, UITableViewDelegate, UITableViewDa
         return 1
     }
     
- 
+    @IBAction func addTaped(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func signOut(_ sender: UIBarButtonItem) {
+        
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print(error.localizedDescription)
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
+
+
--- ToDo + FireBase/Controllers/LoginViewController.swift
+++ ToDo + FireBase/Controllers/LoginViewController.swift
@@ -6,17 +6,85 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
+    
+    let segueIdentifier = "TaskSegue"
 
     @IBOutlet weak var warningLabel: UILabel!
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var registerTextField: UITextField!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        warningLabel.alpha = 0
+        
+        Auth.auth().addStateDidChangeListener {[weak self] (auth, user) in
+            if user != nil{
+                self?.performSegue(withIdentifier: (self?.segueIdentifier)!, sender: nil)
+                
+            }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool){
+        super.viewWillAppear(animated)
+        loginTextField.text = ""
+        registerTextField.text = ""
+    }
+    
     @IBAction func loginButtonAction(_ sender: Any) {
+        guard let email = loginTextField.text, let password = registerTextField.text , email != "" , password != "" else {
+        displayWarningLabel(withText: "Info is incorrect")
+        return
+        }
+        Auth.auth().signIn(withEmail: email, password: password) {[weak self] (user, error) in
+            if error != nil {
+                self?.displayWarningLabel(withText: "Error ocured")
+                return
+            }
+            if user != nil {
+                self?.performSegue(withIdentifier: (self?.segueIdentifier)!, sender: nil)
+                return
+            }
+            self?.displayWarningLabel(withText: "No such user")
+        }
     }
     
     @IBAction func registerButtonAction(_ sender: Any) {
+        guard let email = loginTextField.text, let password = registerTextField.text , email != "" , password != "" else {
+        displayWarningLabel(withText: "Info is incorrect")
+        return
+        }
+        Auth.auth().createUser(withEmail: email, password: password, completion: {(user, error) in
+            if error == nil {
+                if user != nil {
+                    //self?.performSegue(withIdentifier: "TaskSegue", sender: nil)
+                } else {
+                    print("user is not created")
+                }
+            } else {
+                print(error!.localizedDescription)
+            }
+        })
+    }
+
+    
+//MARK: - Methods
+    
+    func displayWarningLabel(withText text: String) {
+        warningLabel.text = text
+        
+        UIView.animate(withDuration: 3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: UIView.AnimationOptions.curveEaseOut, animations: { [weak self] in
+            self?.warningLabel.alpha = 1
+        }, completion: {[weak self] compelte in
+            self?.warningLabel.alpha = 0
+        })
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
     
 }
